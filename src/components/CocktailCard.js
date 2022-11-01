1ef1179a16405ee90e8991770dc427eba4990538
trevor dewindt <trevor.dewindt@gmail.com>
2022-10-31 20:10:01 -0600

    create card template

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,6 +1,18 @@
+import { Card, Button } from 'react-bootstrap'
+
 function CocktailCard(){
     return(
-        <p>Cocktail</p>
+        <div>
+            <Card className='card'>
+                <Card.Img variant="top" src="test" alt="this image" />
+                <Card.Body>
+                    <Card.Text>
+                        Text
+                    </Card.Text>
+                    <Button variant="light">Details</Button> <Button variant="light">Delete</Button>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
