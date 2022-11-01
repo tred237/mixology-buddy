cf5585eea456e44053f2e4a9f69cd5767c0aaad4
trevor dewindt <trevor.dewindt@gmail.com>
2022-10-31 18:42:09 -0600

    test data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  const [test, useTest] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/cocktails')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
+
   return (
     <div>
       <h1>Mixology Buddy</h1>
