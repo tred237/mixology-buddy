5868a67e7aebd445089bb9843f7a0b98612b296b
trevor dewindt <trevor.dewindt@gmail.com>
2022-11-01 17:53:11 -0600

    add measurements and column structure to ingredients component

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,10 +1,20 @@
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Ingredients() {
     return(
-        <Form.Group className="mb-3">
-            <Form.Label>Ingedient</Form.Label>
-            <Form.Control type="text" placeholder="Enter ingredient" />
+        <Form.Group className="mb-3" as={Col}>
+            <Row>
+                <Col>
+                    <Form.Label>Ingedient</Form.Label>
+                    <Form.Control type="text" placeholder="Enter ingredient" />
+                </Col>
+                <Col>
+                    <Form.Label>Measurement</Form.Label>
+                    <Form.Control type="text" placeholder="Enter measurement" />
+                </Col>
+            </Row>
         </Form.Group>
     )
 }
