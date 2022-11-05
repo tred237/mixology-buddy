d6cb7099acc1f143af0d16e30f4e1ad1315f1eb8
trevor dewindt <trevor.dewindt@gmail.com>
2022-11-04 23:26:14 -0600

    add detail button click  event listener

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,8 +1,11 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import { useHistory } from 'react-router-dom'
 
 
 function CocktailCard({ cocktail, onCocktailDelete }){
+    const history = useHistory()
+
     function handleDeleteClick(e){
         fetch(`http://localhost:3000/cocktails/${cocktail.id}`, {
             method: 'DELETE',
@@ -20,7 +23,7 @@ function CocktailCard({ cocktail, onCocktailDelete }){
                 <Card.Img variant="top" src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
                 <Card.Body>
                     <Card.Text>{cocktail.strDrink}</Card.Text>
-                    <Button variant='dark'>Details</Button> <Button variant='dark' onClick={handleDeleteClick} value="Delete">Delete</Button>
+                    <Button variant='dark' onClick={() => history.push(`/details/${cocktail.id}`)}>Details</Button> <Button variant='dark' onClick={handleDeleteClick} value="Delete">Delete</Button>
                 </Card.Body>
             </Card>
     )
