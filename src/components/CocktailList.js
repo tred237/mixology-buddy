f5866635840603d2f60158ba14d3613ebbcf14cc
trevor dewindt <trevor.dewindt@gmail.com>
2022-11-05 12:50:22 -0600

    create grid layout for cards

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,10 +1,19 @@
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 import CocktailCard from './CocktailCard'
 
 function CocktailList({ cocktails, onCocktailDelete }) {
     return(
-        <div>
+        <div id='card-list-container'>
             <h2>Cocktail List</h2>
-            {cocktails.map(cocktail => <CocktailCard key={cocktail.id} cocktail={cocktail} onCocktailDelete={onCocktailDelete} />)}
+            <Row md={3}>
+                {cocktails.map(cocktail => {
+                   return( <Col>
+                            <CocktailCard key={cocktail.id} cocktail={cocktail} onCocktailDelete={onCocktailDelete} />
+                           </Col>)
+                })}
+            </Row>
         </div>
     )
 }
