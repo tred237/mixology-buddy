52c7dae786b9a46757620e63e512464cea035d9f
trevor dewindt <trevor.dewindt@gmail.com>
2022-11-13 13:01:26 -0700

    clean up

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,18 +7,6 @@ import Card from 'react-bootstrap/Card'
 function CocktailCard({ cocktail, onCocktailDelete }){
     const history = useHistory()
 
-    // function handleDeleteClick(e){
-    //     fetch(`http://localhost:3000/cocktails/${cocktail.id}`, {
-    //         method: 'DELETE',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(cocktail)
-    //     })
-    //     .then(res => res.json())
-    //     .then(() => onCocktailDelete(cocktail.id))
-    // }
-
     return(
         <Card>
             <Card.Img variant="top" src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
